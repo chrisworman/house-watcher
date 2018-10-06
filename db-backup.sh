5297e16ac60bfc4b0e73d14c1fd376530af87be8
Chris Worman <chrisworman@gmail.com>
2018-10-05 20:13:51 -0700

    DB backups go to ./dbBackups; deleted old backups

--- db-backup.sh
+++ db-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 timestamp=`date +%Y-%m-%d.%H:%M:%S`
-filename="houses-backup-$timestamp.json"
+filename="./dbBackups/houses-backup-$timestamp.json"
 mongoexport --db house-watcher --collection houses --out "$filename"
